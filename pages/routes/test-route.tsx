b086e438831f4a47f767d6aa31a26a3b26c32714
Bluette1 <marylene.sawyer@gmail.com>
2021-09-08 13:10:37 +0200

    Add Layout
    Add testing route

---
+++ pages/routes/test-route.tsx
@@ -0,0 +1,11 @@
+import Layout from '../../components/layout';
+
+const TestRoute = () => {
+  return (
+    <Layout>
+      <h2 className="invisible">Test Route</h2>
+    </Layout>
+  );
+};
+
+export default TestRoute;

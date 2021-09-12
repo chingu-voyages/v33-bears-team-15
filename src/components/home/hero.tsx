c4274ce5cd58969212c4a1be56f8d65bee72e313
Kerosz <andrei_chirila@outlook.com>
2021-09-12 22:57:07 +0300

    chore: component tweeks and cleanup

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,19 +1,20 @@
 import Container from "~/components/ui/container";
+import Link from "../common/link";
 
 export default function Hero() {
   return (
     <section className="bg-hero h-hero -translate-y-24 bg-no-repeat bg-center bg-cover flex items-center pt-20">
       <Container>
-        <h1 className="text-5xl font-bold max-w-lg pb-4 text-gray-50">
+        <h1 className="text-5xl font-bold max-w-lg pb-4 text-gray-50 leading-[3.45rem]">
           Endless entertainment and knowledge
         </h1>
-        <p className="text-xl text-primary font-bold pb-16">Read thousands of books</p>
-        <button
-          type="button"
-          className="bg-gray-50 text-gray-800 uppercase hover:border-gray-300 hover:shadow hover:opacity-90 hover:text-gray-900 border border-gray-200 py-3.5 px-12 flex justify-center items-center rounded-sm font-bold tracking-wide"
+        <p className="text-xl text-primary font-bold pb-16">Read anytime, anywhere.</p>
+        <Link
+          href="/signup"
+          className="bg-gray-100 text-gray-800 uppercase hover:border-gray-300 hover:shadow hover:opacity-90 hover:text-gray-900 border border-gray-200 py-3.5 px-12 flex justify-center items-center rounded-sm font-bold tracking-wide"
         >
           Get started for free
-        </button>
+        </Link>
       </Container>
     </section>
   );

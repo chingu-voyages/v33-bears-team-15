7292c36e972323c98da091eb7fd2ceefe516921b
Kerosz <andrei_chirila@outlook.com>
2021-09-12 23:52:36 +0300

    refactor: extract theme logic into useTheme hook

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,38 +4,10 @@ import MoonIcon from "~/assets/icons/moonIcon";
 import SunIcon from "~/assets/icons/sunIcon";
 import Link from "./common/link";
 import Container from "./ui/container";
+import useTheme from "~/hooks/use-theme";
 
-// @TODO We need a better API to work with
-// `localStorage`. Need to add to a `utils`
 export default function Header(): JSX.Element {
-  const [isDark, setIsDark] = useState(true);
-
-  // @TODO We need to extract theaming logic into a hook
-  const handleThemeSwitch = (type: "dark" | "light") => {
-    if (type === "dark") {
-      document.documentElement.classList.add("dark");
-
-      localStorage.setItem("theme", "dark");
-      localStorage.setItem("checked", JSON.stringify(true));
-      setIsDark(true);
-    } else {
-      document.documentElement.classList.remove("dark");
-
-      localStorage.setItem("theme", "light");
-      localStorage.setItem("checked", JSON.stringify(false));
-      setIsDark(false);
-    }
-  };
-
-  useEffect(() => {
-    if (localStorage.getItem("theme") === "dark") {
-      document.documentElement.classList.add(localStorage.getItem("theme"));
-    }
-
-    if (localStorage.getItem("checked")) {
-      setIsDark(JSON.parse(localStorage.getItem("checked")));
-    }
-  }, []);
+  const { toggle, isDark } = useTheme();
 
   return (
     <header className="z-30 h-24 relative">
@@ -46,11 +18,11 @@ export default function Header(): JSX.Element {
 
         <div className="flex items-center">
           {isDark ? (
-            <button type="button" onClick={() => handleThemeSwitch("light")}>
+            <button type="button" onClick={() => toggle("light")}>
               <SunIcon className="w-6 text-gray-50" />
             </button>
           ) : (
-            <button type="button" onClick={() => handleThemeSwitch("dark")}>
+            <button type="button" onClick={() => toggle("dark")}>
               <MoonIcon className="w-6 text-gray-50" />
             </button>
           )}
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,10 +1,15 @@
-import Layout from "~/components/layout";
+import Image from "next/image";
+import Link from "~/components/common/link";
 import Container from "~/components/ui/container";
 
 export default function Signin() {
   return (
-    <Layout>
-      <Container>Sign In</Container>
-    </Layout>
+    <section className="bg-hero h-hero -translate-y-24 bg-no-repeat bg-center bg-cover flex items-center pt-20">
+      <Container>
+        <Link href="/">
+          <Image src="/images/logo.png" width={200} height={48} />
+        </Link>
+      </Container>
+    </section>
   );
 }

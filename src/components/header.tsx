f5abf7ab20294ecae889f25f37b44187e04a8fdc
Kerosz <andrei_chirila@outlook.com>
2021-09-12 19:57:46 +0300

    chore: add hero image & header tweeks & add inital signin page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import MoonIcon from "~/assets/icons/moonIcon";
 import SunIcon from "~/assets/icons/sunIcon";
+import Link from "./common/link";
 import Container from "./ui/container";
 
 // @TODO We need a better API to work with
 // `localStorage`. Need to add to a `utils`
 export default function Header(): JSX.Element {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(true);
 
   // @TODO We need to extract theaming logic into a hook
   const handleThemeSwitch = (type: "dark" | "light") => {
@@ -38,7 +39,9 @@ export default function Header(): JSX.Element {
   return (
     <header className="h-20">
       <Container className="flex justify-between items-center h-full">
-        <h1 className="text-2xl font-bold text-gray-700 dark:text-gray-50">Setism</h1>
+        <Link href="/">
+          <h1 className="text-2xl font-bold text-gray-700 dark:text-gray-50">Setism</h1>
+        </Link>
 
         <div className="flex items-center">
           {isDark ? (
@@ -51,9 +54,9 @@ export default function Header(): JSX.Element {
             </button>
           )}
 
-          <button type="button" className="ml-6">
+          <Link href="/signin" className="ml-6">
             Sign In
-          </button>
+          </Link>
         </div>
       </Container>
     </header>

74b519220de91e391df00f5e68b828e8bd53c32a
Bluette1 <marylene.sawyer@gmail.com>
2021-09-13 11:54:38 +0200

    chore: test `npx lint-staged`
    issue #38

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,11 +1,11 @@
-import * as Yup from "yup";
+import * as Yup from 'yup';
 
 export const SIGNUP_SCHEMA = Yup.object().shape({
   email: Yup.string()
-    .email("Email address has a wrong format")
-    .required("Email address is a required field"),
+    .email('Email address has a wrong format!')
+    .required('Email address is a required field!'),
   password: Yup.string()
-    .min(6, "Password must be at least 6 characters long!")
-    .max(24, "Password must be 24 characters at most!")
-    .required("Password is a required field"),
+    .min(6, 'Password must be at least 6 characters long!')
+    .max(24, 'Password must be 24 characters at most!')
+    .required('Password is a required field!'),
 });

1dd44cdac3bf9c3db75d5b728c881aaa7acca800
Andrei Chirila <andrei_chirila@outlook.com>
2021-09-13 20:36:16 +0300

    feat(#53): login page completed (#58)
    * refactor: changed various style convention & configs
    
    * feat: added base layout components & cleanup
    
    * feat: add initial tests
    
    * chore: update project configs
    
    * chore: update tailwind styles
    
    * feat: added error-boundary & link components
    
    * refactor: update header to use icons for theming & config and style fixes
    
    * chore: add hero image & header tweeks & add inital signin page
    
    * chore: update images & add fonts & tweek tailwind config
    
    * feat: add hero
    
    * chore: component tweeks and cleanup
    
    * refactor: extract theme logic into useTheme hook
    
    * feat: add input component & improved header
    
    * feat: add button component & icons
    
    * feat: add signin page & configs
    
    * chore: improved input and button components
    
    * feat: add signin validation

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,5 +1,5 @@
-import cn from "classnames";
-import { forwardRef, ComponentProps, JSXElementConstructor } from "react";
+import cn from 'classnames';
+import { forwardRef, ComponentProps, JSXElementConstructor } from 'react';
 
 enum Variant {
   primary,
@@ -12,7 +12,7 @@ enum Size {
   full,
 }
 
-export interface ButtonProps extends ComponentProps<"button"> {
+export interface ButtonProps extends ComponentProps<'button'> {
   label?: string;
   href?: string;
   as?: string | JSXElementConstructor<any>;
@@ -24,16 +24,16 @@ export interface ButtonProps extends ComponentProps<"button"> {
   reset?: boolean;
 }
 
-const DEFAULT_TAG = "button";
+const DEFAULT_TAG = 'button';
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
   const {
-    type = "button",
+    type = 'button',
     label,
     as: Component = DEFAULT_TAG,
     className,
-    variant = "primary",
-    size = "full",
+    variant = 'primary',
+    size = 'full',
     disabled,
     children,
     loading,
@@ -42,30 +42,33 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
   } = props;
 
   const primaryClass =
-    "bg-green-600 text-gray-50 hover:bg-primary-900 border border-green-700 hover:border-green-600";
+    'bg-primaryBtn shadow-sm hover:shadow-md text-gray-800 hover:text-black hover:bg-primary-900 border border-green-400';
   const googleClass =
-    "bg-gray-100 text-gray-500 border border-gray-300 hover:text-gray-700";
+    'bg-gray-100 text-gray-500 shadow-sm hover:shadow-md border border-gray-300 hover:text-gray-700';
   const twitterClass =
-    "bg-blue-500 border border-blue-500 text-white-normal hover:text-gray-200";
+    'bg-blue-500 border border-blue-500 text-white-normal hover:text-gray-200';
   const modernClass =
-    "bg-gray-50 text-gray-800 uppercase hover:border-gray-300 hover:shadow hover:bg-gray-100 hover:text-gray-700 border border-gray-200";
+    'bg-gray-50 text-gray-800 uppercase hover:border-gray-300 hover:shadow hover:bg-gray-100 hover:text-gray-700 border border-gray-200';
 
   const rootClass = cn(
-    { "rounded-md font-semibold": !reset },
     {
-      [primaryClass]: variant === "primary",
-      [googleClass]: variant === "google",
-      [twitterClass]: variant === "twitter",
-      [modernClass]: variant === "modern",
-      "w-full py-2.5 flex justify-center items-center": size === "full",
-      "opacity-25": disabled,
+      'rounded-md font-semibold transition duration-150 focus:outline-none focus:ring-2 ring-gray-700':
+        !reset,
+    },
+    {
+      [primaryClass]: variant === 'primary',
+      [googleClass]: variant === 'google',
+      [twitterClass]: variant === 'twitter',
+      [modernClass]: variant === 'modern',
+      'w-full py-2.5 flex justify-center items-center': size === 'full',
+      'opacity-25': disabled,
     },
     className
   );
 
   return (
     <Component type={type} ref={ref} className={rootClass} disabled={disabled} {...rest}>
-      {loading ? "Loading..." : label || children}
+      {loading ? 'Loading...' : label || children}
     </Component>
   );
 });

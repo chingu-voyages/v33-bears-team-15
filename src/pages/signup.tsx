104e6e3caa6ef6b2d946084056caba1b8f20df8e
Kerosz <andrei_chirila@outlook.com>
2021-09-13 23:12:35 +0300

    feat: add completed signup page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,11 +1,13 @@
+import Image from 'next/image';
 import { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 
+import advantages from '~data/advantages';
 import GoogleIcon from '~/assets/icons/googleIcon';
 import MailIcon from '~/assets/icons/mailIcon';
+import CheckCircleIcon from '~/assets/icons/checkCircleIcon';
 import Link from '~/components/common/link';
-import Layout from '~/components/layout';
 import Button from '~/components/ui/button';
 import Container from '~/components/ui/container';
 import Input from '~/components/ui/input';
@@ -50,84 +52,122 @@ export default function Signup() {
   };
 
   return (
-    <Layout headerProps={{ withBorder: true }}>
-      <section className="py-20">
-        <Container maxW="max-w-md" className="flex flex-col items-center px-12">
-          <h1 className="text-4xl font-bold pb-8">Join the most epic platform</h1>
+    <section className="min-h-screen relative flex items-center">
+      <div
+        className="absolute lg:block hidden h-full w-1/2 top-0 left-0 z-0 bg-gray-800 border-r border-gray-700"
+        aria-hidden
+      />
 
-          <form onSubmit={handleSubmit(onSubmitHandler)} className="w-full">
-            {serverErrorState && (
-              <span
-                role="alert"
-                className="block text-sm dark:text-red-500 text-red-700 mb-2.5 pl-0.5"
-              >
-                {serverErrorState}
-              </span>
-            )}
+      <Container className="py-10">
+        <Link href="/" title="Dekoo branding">
+          <Image src="/images/logo.png" width={176} height={42} />
+        </Link>
 
-            <div className="mb-3.5">
-              <label htmlFor="email" className="sr-only">
-                Email address
-              </label>
+        <div className="lg:grid grid-cols-2 lg:mt-8 mt-12 flex flex-col-reverse items-center lg:items-start">
+          <div className="z-10 lg:mt-0 mt-6">
+            <dl className="pt-10 max-w-sm space-y-12">
+              {advantages.map(({ name, description }) => (
+                <div className="relative" key={name}>
+                  <dt>
+                    <div className="absolute flex items-center justify-center">
+                      <CheckCircleIcon
+                        className="w-8 text-primary-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <p className="ml-14 text-2xl leading-6 font-bold text-black-normal">
+                      {name}
+                    </p>
+                  </dt>
+                  <dd className="mt-4 ml-14 text-lg font-medium text-gray-300">
+                    {description}
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
 
-              <Input
-                type="text"
-                name="email"
-                id="email"
-                autoComplete="email"
-                placeholder="Email Address"
-                aria-invalid={!!errors.email}
-                isError={errors.email && touchedFields.email}
-                error={errors.email?.message}
-                {...register('email')}
-              />
-            </div>
+          <div className="flex flex-col lg:pl-14 max-w-sm">
+            <h2
+              className="sm:text-5xl text-4xl font-semibold mb-12"
+              style={{ lineHeight: 1.15 }}
+            >
+              Join the most epic platform
+            </h2>
+            <form onSubmit={handleSubmit(onSubmitHandler)} className="w-full">
+              {serverErrorState && (
+                <span
+                  role="alert"
+                  className="block text-sm dark:text-red-500 text-red-700 mb-2.5 pl-0.5"
+                >
+                  {serverErrorState}
+                </span>
+              )}
 
-            <div className="mb-3.5">
-              <label htmlFor="password" className="sr-only">
-                Password
-              </label>
+              <div className="mb-3.5">
+                <label htmlFor="email" className="sr-only">
+                  Email address
+                </label>
 
-              <Input
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                placeholder="Password"
-                showPasswordToggle
-                aria-invalid={!!errors.password}
-                isError={errors.password && touchedFields.password}
-                error={errors.password?.message}
-                {...register('password')}
-              />
-            </div>
+                <Input
+                  type="text"
+                  name="email"
+                  id="email"
+                  autoComplete="email"
+                  placeholder="Email Address"
+                  aria-invalid={!!errors.email}
+                  isError={errors.email && touchedFields.email}
+                  error={errors.email?.message}
+                  {...register('email')}
+                />
+              </div>
 
-            <div className="mb-3.5">
-              <label htmlFor="fullName" className="sr-only">
-                Full Name
-              </label>
+              <div className="mb-3.5">
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
 
-              <Input
-                type="text"
-                id="fullName"
-                autoComplete="given-name"
-                placeholder="Full Name"
-                aria-invalid={!!errors.fullName}
-                isError={errors.fullName && touchedFields.fullName}
-                error={errors.fullName?.message}
-                {...register('fullName')}
-              />
-            </div>
+                <Input
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  placeholder="Password"
+                  showPasswordToggle
+                  aria-invalid={!!errors.password}
+                  isError={errors.password && touchedFields.password}
+                  error={errors.password?.message}
+                  {...register('password')}
+                />
+              </div>
 
-            <Button type="submit" variant="primary" size="full" className="mb-3.5">
-              <MailIcon className="w-6 mr-2" />{' '}
-              {isSubmitting ? 'Loading...' : 'Continue with Email'}
-            </Button>
+              <div className="mb-3.5">
+                <label htmlFor="fullName" className="sr-only">
+                  Full Name
+                </label>
 
-            <Button variant="google" size="full">
-              <GoogleIcon className="w-6 mr-2" /> Continue with Google
-            </Button>
+                <Input
+                  type="text"
+                  id="fullName"
+                  autoComplete="given-name"
+                  placeholder="Full Name"
+                  aria-invalid={!!errors.fullName}
+                  isError={errors.fullName && touchedFields.fullName}
+                  error={errors.fullName?.message}
+                  {...register('fullName')}
+                />
+              </div>
 
-            <p className="mt-6">
+              <Button type="submit" variant="primary" size="full" className="mb-3.5">
+                <MailIcon className="w-6 mr-2" />{' '}
+                {isSubmitting ? 'Loading...' : 'Continue with Email'}
+              </Button>
+
+              <Button variant="google" size="full">
+                <GoogleIcon className="w-6 mr-2" /> Continue with Google
+              </Button>
+            </form>
+
+            <p className="mt-8">
               By clicking continue, you agree to our{' '}
               <Link
                 href="/legal/terms"
@@ -147,7 +187,7 @@ export default function Signup() {
               .
             </p>
 
-            <p className="mt-14 w-full pt-5 border-t text-lg border-gray-300 dark:border-gray-700 text-gray-800 dark:text-gray-50 font-semibold text-center">
+            <p className="mt-14 w-full pt-5 border-t text-lg border-gray-300 dark:border-gray-700 text-gray-800 dark:text-gray-50 font-semibold">
               Already have an account?
               <Link
                 href="/signin"
@@ -157,9 +197,9 @@ export default function Signup() {
                 Sign In
               </Link>
             </p>
-          </form>
-        </Container>
-      </section>
-    </Layout>
+          </div>
+        </div>
+      </Container>
+    </section>
   );
 }

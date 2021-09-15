ad92771ec63a84c35555d84b1773337634cb115e
Kerosz <andrei_chirila@outlook.com>
2021-09-15 18:54:23 +0300

    feat: changed carousel to react-slick

--- src/components/library/slider.tsx
+++ src/components/library/slider.tsx
@@ -1,27 +1,66 @@
+// /* eslint-disable import/no-unresolved */
+
 import Image from 'next/image';
+import Slick, { Settings } from 'react-slick';
+import { Dispatch, SetStateAction, useState } from 'react';
 
 import Container from '~/components/ui/container';
-import Swiper from '../swiper';
-import slides from '~data/slides';
+import ChevronLeftIcon from '~/assets/icons/chevronLeftIcon';
+import ChevronRightIcon from '~/assets/icons/chevronRightIcon';
+import slides, { SlideType } from '~data/slides';
+
+import 'slick-carousel/slick/slick.css';
+import Button from '../ui/button';
+
+interface ISlide extends SlideType {
+  sliderRef: Slick | null;
+}
+
+type SlickOptions = Settings & { ref: Dispatch<SetStateAction<Slick>> };
+
+function Slide({ name, color, src, sliderRef }: ISlide) {
+  return (
+    <div
+      style={{ backgroundColor: color }}
+      className="text-gray-700 flex items-center py-12"
+    >
+      <Container className="flex" maxW="max-w-7xl">
+        <button type="button" onClick={sliderRef?.slickNext}>
+          <ChevronLeftIcon className="w-14 mr-3.5" strokeWidth={1} />
+        </button>
+
+        <Image src={src} alt={name} width={258} height={258} />
+        <div className="pl-12 w-8/12">
+          <h3 className="text-[2.55rem] leading-[3.35rem] font-semibold">{name}</h3>
+        </div>
+
+        <button type="button" onClick={sliderRef?.slickPrev}>
+          <ChevronRightIcon className="w-14 ml-3.5" strokeWidth={1} />
+        </button>
+      </Container>
+    </div>
+  );
+}
 
 export default function Slider() {
-  return (
-    <Swiper>
-      {slides.map(({ name, color, src }) => (
-        <Swiper.Slide
-          style={{ backgroundColor: color }}
-          className="text-gray-700 flex items-center py-12"
-          key={`swiper__slide-${name}`}
-        >
-          <Container className="flex" maxW="max-w-6xl">
-            <Image src={src} alt={name} width={258} height={258} />
+  const [sliderRef, setSliderRef] = useState<Slick | null>(null);
 
-            <div className="w-7/12 pl-12">
-              <h3 className="text-4xl leading-[3.45rem] font-bold">{name}</h3>
-            </div>
-          </Container>
-        </Swiper.Slide>
+  const defaultOptions: SlickOptions = {
+    ref: setSliderRef,
+    dots: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    swipeToSlide: true,
+    fade: true,
+  };
+
+  return (
+    <Slick {...defaultOptions} className="relative">
+      {slides.map((s: SlideType) => (
+        <Slide key={`slick__slide-${s.name}`} {...s} sliderRef={sliderRef} />
       ))}
-    </Swiper>
+    </Slick>
   );
 }

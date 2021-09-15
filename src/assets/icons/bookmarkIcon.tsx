85852081eda1ce34bcec555c9328c6a603c9c308
Kerosz <andrei_chirila@outlook.com>
2021-09-15 20:26:54 +0300

    chore: update button component & add bookmark icon

---
+++ src/assets/icons/bookmarkIcon.tsx
@@ -0,0 +1,24 @@
+import type { ComponentPropsWithoutRef } from 'react';
+
+export type IBookmarkIcon = ComponentPropsWithoutRef<'svg'>;
+
+function BookmarkIcon({ strokeWidth = 2, ...rest }: IBookmarkIcon): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      {...rest}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={strokeWidth}
+        d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+      />
+    </svg>
+  );
+}
+
+export default BookmarkIcon;

b6c4013f14b12a5512aaa05a0f12dd045acac6e1
Bluette1 <marylene.sawyer@gmail.com>
2021-09-15 18:47:17 +0200

    chore: add footer content

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,45 @@
+import Link from './common/link';
+
 export default function Footer(): JSX.Element {
-  return <footer className="text-center pt-44">Footer</footer>;
+  return (
+    <section className="w-full flex justify-center pt-44">
+      <div>
+        <div>
+          <ul className="flex list-none justify-around">
+            <Link href="/ ">
+              <li className="px-7">Home</li>
+            </Link>
+            <Link href="/ ">
+              <li className="px-7">About</li>
+            </Link>
+            <Link href="/ ">
+              <li className="px-7">Authors</li>
+            </Link>
+            <Link href="/ ">
+              <li className="px-7">Publishers</li>
+            </Link>
+            <Link href="/ ">
+              <li className="px-7">FAQ</li>
+            </Link>
+            <Link href="/signin">
+              <li className="px-7">Sign in</li>
+            </Link>
+            <Link href="/ ">
+              <li className="px-7">Read</li>
+            </Link>
+            <Link href="/ ">
+              <li className="px-7">Privacy Statement</li>
+            </Link>
+            <Link href="/ ">
+              <li>Terms of Service</li>
+            </Link>
+          </ul>
+        </div>
+        <p className="text-center py-6">
+          DECKOO © 2021 | All rights reserved. Terms under which this service is provided
+          to you. Privacy Policy.
+        </p>
+      </div>
+    </section>
+  );
 }

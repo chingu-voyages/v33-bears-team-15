c2876635ee66abcb8c430f9b88ab4b8e04636bc8
Kerosz <andrei_chirila@outlook.com>
2021-09-17 11:25:11 +0300

    chore: tweeked bookReview section

--- src/components/home/bookReview.tsx
+++ src/components/home/bookReview.tsx
@@ -4,24 +4,23 @@ import Button from '~/components/ui/button';
 
 export default function BookReview() {
   return (
-    <section className="pt-20 pb-10">
-      <h4 className="text-5xl w-full py-7 text-center font-bold">Read a book</h4>
-      <Container className="md:justify-center px-0 sm:px-0 md:px-0 lg:px-0 xl:px-0 flex-wrap flex">
-        <div className="p-4 w-full md:w-1/2 lg:w-1/2 ">
-          <div>
-            <Image src="/images/motor-engines.jpeg" height="685" width="600" />
-          </div>
+    <section className="pt-20 pb-16">
+      <Container className="md:justify-center flex lg:flex-row flex-col-reverse">
+        <div className="xl:px-24 lg:px-16 w-full lg:w-1/2 flex justify-center lg:mt-0 mt-12">
+          <Image src="/images/motor-engines.jpeg" height="685" width="510" />
         </div>
-        <div className="p-4 w-full lg:h-featured md:w-1/2 lg:w-1/3 ">
-          <h5 className="text-2xl text-primary-700 py-7 font-bold">Book Review</h5>
-          <h5 className="text-3xl text-primary-700 py-7 font-bold">Mortal Engines</h5>
-          <p className="text-2xl">
+        <div className="sm:pl-16 pl-0 w-full lg:w-1/2 ">
+          <p className="uppercase font-semibold text-lg pb-5 lg:mt-24">Book Review</p>
+          <h2 className="text-6xl text-primary-700 pb-6 font-semibold">Mortal Engines</h2>
+          <p className="text-xl font-semibold">
             In a post-apocalyptic era where towns are ever changing to survive, two aliens
             come together to stop a dark and sinister conspirancy.
           </p>
-          <h5 className="text-2xl py-5 font-bold">Roseburg Koo</h5>
-          <Button variant="read" size="normal">
-            Read Now
+          <p className="text-2xl py-5 font-semibold text-gray-600 dark:text-gray-50">
+            Roseburg Koo
+          </p>
+          <Button variant="outlined" size="normal">
+            Start Reading Now
           </Button>
         </div>
       </Container>

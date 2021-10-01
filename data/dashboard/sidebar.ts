1b9dccd58bbd4e5b82fcd91abf3eb83af6cbe592
Kerosz <andrei_chirila@outlook.com>
2021-10-02 02:28:44 +0300

    chore: add dashboard data

--- data/dashboard/sidebar.ts
+++ data/dashboard/sidebar.ts
@@ -1,50 +1,44 @@
-const sidebarData = {
-  subCategories: [
-    { name: 'Totes', href: '#' },
-    { name: 'Backpacks', href: '#' },
-    { name: 'Travel Bags', href: '#' },
-    { name: 'Hip Bags', href: '#' },
-    { name: 'Laptop Sleeves', href: '#' },
-  ],
-  filters: [
-    {
-      id: 'color',
-      name: 'Color',
-      options: [
-        { value: 'white', label: 'White', checked: false },
-        { value: 'beige', label: 'Beige', checked: false },
-        { value: 'blue', label: 'Blue', checked: true },
-        { value: 'brown', label: 'Brown', checked: false },
-        { value: 'green', label: 'Green', checked: false },
-        { value: 'purple', label: 'Purple', checked: false },
-      ],
-    },
-    {
-      id: 'category',
-      name: 'Category',
-      options: [
-        { value: 'new-arrivals', label: 'New Arrivals', checked: false },
-        { value: 'sale', label: 'Sale', checked: false },
-        { value: 'travel', label: 'Travel', checked: true },
-        { value: 'organization', label: 'Organization', checked: false },
-        { value: 'accessories', label: 'Accessories', checked: false },
-      ],
-    },
-    {
-      id: 'size',
-      name: 'Size',
-      options: [
-        { value: '2l', label: '2L', checked: false },
-        { value: '6l', label: '6L', checked: false },
-        { value: '12l', label: '12L', checked: false },
-        { value: '18l', label: '18L', checked: false },
-        { value: '20l', label: '20L', checked: false },
-        { value: '40l', label: '40L', checked: true },
-      ],
-    },
-  ],
-} as const;
+import BookIcon from '~/assets/icons/bookIcon';
+import HomeIcon from '~/assets/icons/homeIcon';
 
-export type SidebarDataType = typeof sidebarData;
+export type SidebarMenuType = {
+  name: string;
+  subMenus: Array<{ label: string; href: string }>;
+  icon: any;
+  href: string | null;
+};
+
+export type SidebarType = {
+  listTitle: string;
+  menus: SidebarMenuType[];
+};
+
+const sidebarData: SidebarType[] = [
+  {
+    listTitle: 'General',
+    menus: [
+      {
+        name: 'Home',
+        subMenus: [],
+        icon: HomeIcon,
+        href: '/dashboard/home',
+      },
+    ],
+  },
+  {
+    listTitle: 'Management',
+    menus: [
+      {
+        name: 'Books',
+        subMenus: [
+          { label: 'List', href: '/' },
+          { label: 'Create', href: '/' },
+        ],
+        icon: BookIcon,
+        href: null,
+      },
+    ],
+  },
+];
 
 export default sidebarData;

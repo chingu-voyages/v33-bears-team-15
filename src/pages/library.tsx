853620fb641df8cd04fdf4f3ff86c07ff009efa9
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-10-06 19:18:45 -0400

    chore: removed book page import

--- src/pages/library.tsx
+++ src/pages/library.tsx
@@ -3,7 +3,6 @@ import Carousel from '~/components/library/carousel';
 import Slider from '~/components/library/slider';
 import Container from '~/components/ui/container';
 import booksByCategories from '~data/books';
-import Book from './book';
 
 export default function Library() {
   return (
@@ -17,13 +16,6 @@ export default function Library() {
           <Carousel key={c.category} title={c.category} data={c.data} moreHref={c.link} />
         ))}
       </Container>
-      <Book
-        name="Lean Project Management Explained"
-        author="Can Akdeniz"
-        src="https://imgv2-2-f.scribdassets.com/img/audiobook_square_badge/399285183/original/216x216/ade11cc8e3/1629025589?v=1"
-        rating={4}
-        description="this is a test description"
-      />
     </Layout>
   );
 }

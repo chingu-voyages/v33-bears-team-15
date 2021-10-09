2cf5f68a862c114f0fdc7764b6aad8500ac76d21
R2D2 <66644638+R2rit0@users.noreply.github.com>
2021-10-09 15:39:23 -0400

    refactor: changed the mock data in the profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -13,11 +13,19 @@ export default function Profile() {
           <Avatar
             src="https://res.cloudinary.com/minimal-ui/image/upload/v1614655910/upload_minimal/avatar/minimal_avatar.jpg"
             title="test"
-            className="mt-56"
+            className="flex-initial"
+            width="300px"
           />
           <div className="flex-initial ml-24">
             <h1>About me</h1>
-            <p>Im cool</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+              non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.{' '}
+            </p>
           </div>
         </div>
         <Carousel title="Reading list" data={mockData[0]} />

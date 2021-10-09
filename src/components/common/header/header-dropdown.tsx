4d48a16193fe8e24b657f1465415edec7f100187
Kerosz <andrei_chirila@outlook.com>
2021-10-09 14:24:16 +0300

    chore: improved header dropdown menu

--- src/components/common/header/header-dropdown.tsx
+++ src/components/common/header/header-dropdown.tsx
@@ -1,10 +1,11 @@
 import { ComponentPropsWithoutRef } from 'react';
-import UserIcon from '~/assets/icons/userIcon';
+
+import Link from '../link';
 import Button from '~/components/ui/button';
 import Dropdown from '~/components/ui/dropdown';
 import List from '~/components/ui/list';
+import UserIcon from '~/assets/icons/userIcon';
 import { IUser } from '~/types';
-import Link from '../link';
 
 export interface IHeaderDropdown extends ComponentPropsWithoutRef<'div'> {
   user: IUser;
@@ -21,31 +22,38 @@ export default function HeaderDropdown({ user, className, logoutFn }: IHeaderDro
         </div>
       }
     >
-      <div className="px-4 py-3.5">
+      <div className="px-5 py-3.5">
         <span className="dark:text-gray-50 text-gray-900 font-semibold text-lg">
           Hi {user.fullName}!
         </span>
       </div>
 
-      <List className="px-2 py-2">
-        {/* {dropdownData.map(({ label, icon: Icon, href }) => (
-          <Dropdown.Item as="li" key={label}>
-            {({ active }) => (
-              <Link
-                href={href}
-                className={`${
-                  active ? 'dark:hover:bg-darkGray hover:bg-gray-100' : ''
-                } flex rounded-md items-center max-w-full px-2 py-2.5`}
-              >
-                <Icon
-                  className="w-6 mr-3 dark:text-gray-400 text-gray-500"
-                  aria-hidden="true"
-                />
-                <span>{label}</span>
-              </Link>
-            )}
-          </Dropdown.Item>
-        ))} */}
+      <List className="px-4 py-2">
+        <Dropdown.Item as="li">
+          {({ active }) => (
+            <Link
+              href="/profile"
+              className={`${
+                active ? 'dark:hover:bg-darkGray hover:bg-gray-100' : ''
+              } flex rounded-md items-center max-w-full px-2 py-2.5`}
+            >
+              <span>Public profile</span>
+            </Link>
+          )}
+        </Dropdown.Item>
+
+        <Dropdown.Item as="li">
+          {({ active }) => (
+            <Link
+              href="/settings"
+              className={`${
+                active ? 'dark:hover:bg-darkGray hover:bg-gray-100' : ''
+              } flex rounded-md items-center max-w-full px-2 py-2.5`}
+            >
+              <span>Your account</span>
+            </Link>
+          )}
+        </Dropdown.Item>
 
         <Dropdown.Item as="li">
           <Button className="mb-2 mt-4 rounded-lg" variant="shadow" onClick={logoutFn}>

6ac0fe889a5a37e41e26f13eb0106ff102ea12f0
Kerosz <andrei_chirila@outlook.com>
2021-10-15 00:56:19 +0300

    chore: add auth and user types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,6 @@ export * from './auth.type';
 export * from './role.enum';
 export * from './store.type';
 export * from './user.type';
+export * from './category.type';
+export * from './author.type';
+export * from './book.type';
